e9016c1706fa86d59063dbc5a1da845b2380eb85
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-09-05 16:43:57 +0300

    Connecting server with Socket.io

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const http = require("http");
 const port = process.env.port || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 //it enables the server to serve static files (e.g., HTML, CSS, JavaScript, images) to clients
 app.use(express.static("public"));
-
-const server = http.createServer(app);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+io.on("connection", (socket) => {
+  console.log("user connected to socket.io server");
+  console.log(socket.id);
+});
 server.listen(port, () => {
   console.log(`listening on Port ${port}`);
 });
