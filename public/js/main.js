137b79556b2aa6817e7eda851a0a700a82d374fd
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-09-21 08:22:52 +0300

    Screen Sharing functionality

--- public/js/main.js
+++ public/js/main.js
@@ -68,3 +68,11 @@ cameraButton.addEventListener("click", () => {
   localStream.getVideoTracks()[0].enabled = !cameraEnabled;
   ui.updateCameraButton(cameraEnabled);
 });
+
+const switchForScreenSharingButton = document.getElementById(
+  "screen_sharing_button"
+);
+switchForScreenSharingButton.addEventListener("click", () => {
+  const screenSharingActive = store.getState().screenSharingActive;
+  webRTCHandler.switchBetweenCameraAndScreenSharing(screenSharingActive);
+});
--- public/js/webRTCHandler.js
+++ public/js/webRTCHandler.js
@@ -200,3 +200,63 @@ export const handleWebRTCCandidate = async (data) => {
     console.error("error occured when trying to add recieved candidate", err);
   }
 };
+
+let screenSharingStream;
+
+export const switchBetweenCameraAndScreenSharing = async (
+  screenSharingActive
+) => {
+  if (screenSharingActive) {
+    const localStream = store.getState().localStream;
+    const senders = peerConnection.getSenders();
+
+    const sender = senders.find((sender) => {
+      return sender.track.kind === localStream.getVideoTracks()[0].kind;
+    });
+
+    if (sender) {
+      sender.replaceTrack(localStream.getVideoTracks()[0]);
+    }
+
+    // stop screen sharing stream
+
+    store
+      .getState()
+      .screenSharingStream.getTracks()
+      .forEach((track) => track.stop());
+
+    store.setScreenSharingActive(!screenSharingActive);
+
+    ui.updateLocalVideo(localStream);
+  } else {
+    console.log("switching for screen sharing");
+    try {
+      screenSharingStream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+      });
+      store.setScreenSharingStream(screenSharingStream);
+
+      // replace track which sender is sending
+      const senders = peerConnection.getSenders();
+
+      const sender = senders.find((sender) => {
+        return (
+          sender.track.kind === screenSharingStream.getVideoTracks()[0].kind
+        );
+      });
+
+      if (sender) {
+        sender.replaceTrack(screenSharingStream.getVideoTracks()[0]);
+      }
+
+      store.setScreenSharingActive(!screenSharingActive);
+
+      ui.updateLocalVideo(screenSharingStream);
+    } catch (err) {
+      console.error(
+        "error occured when trying to get screen sharing stream",
+        err
+      );
+    }
+  }
+};
