e69c5fd60e125ae4654829db5646e2652bfd8dc6
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-09-21 07:20:28 +0300

    Adding functionality to mute and unmute and turn  on_off camera

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ import * as store from "./store.js";
 import * as wss from "./wss.js";
 import * as webRTCHandler from "./webRTCHandler.js";
 import * as constants from "./constants.js";
+import * as ui from "./ui.js";
 
 //initialization of socketIO connection
 const socket = io("/"); // Connect to the server
@@ -48,4 +49,22 @@ personalCodeVideoButton.addEventListener("click", () => {
   const callType = constants.callType.VIDEO_PERSONAL_CODE;
 
   webRTCHandler.sendPreOffer(callType, calleePersonalCode);
+});
+
+// add event listener for video call buttons
+
+const micButton = document.getElementById("mic_button");
+micButton.addEventListener("click", () => {
+  const localStream = store.getState().localStream;
+  const micEnabled = localStream.getAudioTracks()[0].enabled;
+  localStream.getAudioTracks()[0].enabled = !micEnabled;
+  ui.updateMicButton(micEnabled);
+});
+
+const cameraButton = document.getElementById("camera_button");
+cameraButton.addEventListener("click", () => {
+  const localStream = store.getState().localStream;
+  const cameraEnabled = localStream.getVideoTracks()[0].enabled;
+  localStream.getVideoTracks()[0].enabled = !cameraEnabled;
+  ui.updateCameraButton(cameraEnabled);
 });
--- public/js/ui.js
+++ public/js/ui.js
@@ -132,6 +132,24 @@ const showVideoCallElements = () => {
   disableDashboard();
 };
 
+// ui call buttons
+
+const micOnImgSrc = "./utils/images/mic.png";
+const micOffImgSrc = "./utils/images/micOff.png";
+
+export const updateMicButton = (micActive) => {
+  const micButtonImage = document.getElementById("mic_button_image");
+  micButtonImage.src = micActive ? micOffImgSrc : micOnImgSrc;
+};
+
+const cameraOnImgSrc = "./utils/images/camera.png";
+const cameraOffImgSrc = "./utils/images/cameraOff.png";
+
+export const updateCameraButton = (cameraActive) => {
+  const cameraButtonImage = document.getElementById("camera_button_image");
+  cameraButtonImage.src = cameraActive ? cameraOffImgSrc : cameraOnImgSrc;
+};
+
 // ui helper functions
 
 const enableDashboard = () => {
